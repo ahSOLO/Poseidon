a84cb833dbccfd6c88ebad3caee1810dc3a6678a
ahSOLO <junmenghan@gmail.com>
2020-12-07 17:00:37 -0800

    fixed schema_entry field display in admin

--- docs/admin.py
+++ docs/admin.py
@@ -16,10 +16,23 @@ class TemplateSchemaAdmin(admin.ModelAdmin):
 class EntryAdmin(admin.StackedInline):
   model = Entry
   list_display = ['value_short', 'value_long', 'value_bool']
+  # Only display schema entries that correspond to the correct template schema
+  def formfield_for_foreignkey(self, db_field, request=None, **kwargs):
+    field = super(EntryAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
+    if db_field.name == 'schema_entry':
+      if request._obj_ is not None:
+        field.queryset = field.queryset.filter(template_schema = request._obj_.template_schema)  
+      else:
+        field.queryset = field.queryset.none()
+    return field
 
 class EntrySetAdmin(admin.ModelAdmin):
   model = EntrySet
   inlines = [EntryAdmin]
+  # Save obj reference for processing in Inline
+  def get_form(self, request, obj=None, **kwargs):
+    request._obj_ = obj
+    return super(EntrySetAdmin, self).get_form(request, obj, **kwargs)
 
 admin.site.register(Template, TemplateAdmin)
 admin.site.register(TemplateSchema, TemplateSchemaAdmin)
