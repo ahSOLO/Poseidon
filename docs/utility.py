022eca44f6b0ddbb9028b3be9713c7b69987e9de
ahSOLO <junmenghan@gmail.com>
2020-12-09 14:32:23 -0800

    Added comments, fixed a bug relating to pop_schema view

--- docs/utility.py
+++ docs/utility.py
@@ -1,8 +1,10 @@
 from django.core.exceptions import ValidationError
 
+# Path for storing uploaded Templates
 def user_directory_path(instance, filename):
   return 'user_{0}/{1}'.format(instance.user.id, filename)
 
+# Maximum file size for uploaded Templates
 def file_size(value):
     limit = 5 * 1024 * 1024
     if value.size > limit:
--- users/models.py
+++ users/models.py
@@ -8,8 +8,6 @@ class CustomUser(AbstractUser):
   first_name = models.CharField(max_length=50)
   last_name = models.CharField(max_length=50)
   organization = models.CharField(max_length=50, default="", blank=True)
-  # created = models.DateTimeField(auto_now_add=True, editable=False)
-  # last_updated = models.DateTimeField(auto_now=True, editable=False)
 
   def __str__(self):
     return str(self.username)
