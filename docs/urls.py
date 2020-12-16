7eb57ee1f4269b292244a402e737b4ebc04c00dc
ahSOLO <junmenghan@gmail.com>
2020-12-16 13:05:44 -0800

    added dynamic dropdowns to manage_forms page

--- docs/urls.py
+++ docs/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path('edit_form/<schema_id>', views.edit_schema, name='edit_schema'),
     path('pop_form/<schema_id>/<entryset_id>', views.pop_schema, name='edit_entryset'),
     path('pop_form/<schema_id>/', views.pop_schema, name='pop_schema'),
+    path('ajax/load-schemas/', views.load_schemas, name='ajax-load-schemas'),
 ]
